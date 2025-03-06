8ef65960bb81aabc6f8ebd217bf0a23bde689193
zhangbohan <zhangbohan@vv.cn>
2025-03-06 16:13:14 +0800

    feat: 【1892898143626649602】 修改课包fieldKey

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,6 +5,8 @@ import HistoryPage from './pages/HistoryPage';
 import ProfilePage from './pages/ProfilePage';
 import DevTools from './components/DevTools';
 
+let a = 112;
+
 function App() {
   const isDev = import.meta.env.DEV;
 
